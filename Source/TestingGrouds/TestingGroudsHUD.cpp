0db9b45572f722787b1ca4563ff9b57a9581acf8
Daniia <daniia.nigmatullina@gmail.com>
2018-10-10 22:42:35 +0300

    Lecture 243

--- Source/TestingGrouds/TestingGroudsHUD.cpp
+++ Source/TestingGrouds/TestingGroudsHUD.cpp
@@ -10,7 +10,7 @@
 ATestingGroudsHUD::ATestingGroudsHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Player/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/UI/Textures/FirstPersonCrosshair"));
 	CrosshairTex = CrosshairTexObj.Object;
 }
 
